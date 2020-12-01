72855dc204007d478fbc90acc7819fef3e541be0
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2020-12-01 09:06:07 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-basic.forever(function () {
-	
+let sol = 0
+let H_sol = 0
+input.onButtonPressed(Button.A, function () {
+    basic.pause(200)
+    dht11_dht22.queryData(
+    DHTtype.DHT11,
+    DigitalPin.P0,
+    true,
+    true,
+    false
+    )
+    basic.pause(200)
+    basic.showString("Air" + dht11_dht22.readData(dataType.humidity) + "%")
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P7, 1)
+    basic.pause(200)
+    sol = pins.analogReadPin(AnalogPin.P2)
+    H_sol = Math.trunc((sol - 310) * 100 / (1023 - 310))
+    pins.digitalWritePin(DigitalPin.P7, 0)
+    serial.writeValue("sol", H_sol)
+    basic.showString("H sol" + H_sol + "%")
+    basic.pause(1000)
 })
